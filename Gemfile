dbfc19bee6244c73e166d115e65e89f22175edbe
Soenke Ruempler <soenke@jimdo.com>
2014-04-03 22:41:52 +0200

    Use puppet tooling for path discovery
    in order to reduce copy and paste of business logic and avoid bugs like
    found in #2.
    
     - Puppet 2.7 now got a dependency as it's the current Jimdo prod
       version as of writing this.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem "puppet", "~>2.7"
+
 # Specify your gem's dependencies in jimdo-rspec-puppet-helpers.gemspec
 gemspec
--- lib/jimdo-rspec-puppet-helpers.rb
+++ lib/jimdo-rspec-puppet-helpers.rb
@@ -1,4 +1,7 @@
 require 'rspec'
+require 'puppet'
+require 'puppet/file_serving'
+require 'puppet/file_serving/metadata'
 
 RSpec::Matchers.define :puppet_file_contains do |file, expected|
   match do |actual|
@@ -10,48 +13,22 @@ RSpec::Matchers.define :puppet_file_contains do |file, expected|
     end
     source_attr = resource.to_hash[:source]
     if not source_attr then
-      @message = "expected source attribute on #{resource}, found: #{resource.to_hash.keys.join(",")}"
+      @message = "expected source attribute on #{resource}, found: #{resource.to_hash.keys.sort.join(",")}"
       next
     end
-    files = source_attr.map do |catalog_file|
-      catalog_file.gsub(/^puppet:\/\//, '')
-    end
-    found_file = nil
-    files.each do |catalog_file|
-      sliced_path = catalog_file.split('/')
-      module_path = sliced_path[1..2]
-      relative_path_in_puppet_files = sliced_path.slice(3..sliced_path.length)
 
-      # check whether we have rspec fixtures
-      if File.directory?("./spec/fixtures") then
-        path = File.expand_path(
-          File.join(
-            'spec/fixtures',
-            module_path,
-            'files',
-            relative_path_in_puppet_files
-           )
-        );
-      # else use files in puppet source tree
-      else
-        path = File.expand_path(
-          File.join(
-            module_path,
-            'files',
-            relative_path_in_puppet_files
-           )
-        );
-      end
-      if File.file?(path) then
-        found_file = path
-        break
-      end
+    file_data = nil
+    source_attr.each do |source|
+      file_data = Puppet::FileServing::Metadata.indirection.find(source)
+      break if file_data
     end
 
-    if !found_file then
+    if !file_data then
       @message = "no files specified in 'source' exist"
-      false
+      next
     else
+      found_file = file_data.path
+
       @message = "expected that #{file}(source=#{found_file}) matches #{expected}"
       expected.match(IO.read(found_file)) != nil
     end
